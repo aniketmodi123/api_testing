0017642ce94d5f42cca47a26557db1b7a6ef4866
aniketmodi1234 <aniketmodi@polarisgrids.com>
2025-09-04 09:56:58 +0530

    feat: Configure API_BASE for environment variable support

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { handleApiError } from './utils/errorHandler';
 
-export const API_BASE = 'https://api-testing-2vjt.onrender.com';
+export const API_BASE =
+  process.env.REACT_APP_API_BASE || 'https://api-testing-2vjt.onrender.com';
 export const api = axios.create({ baseURL: API_BASE });
 
 // Add request interceptor to automatically add auth headers
